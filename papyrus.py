da3a3afd3e37d2fb5f51a34e4d8e4779ce7ddec5
Zachary Michaelov <zach.michaelov@gmail.com>
2012-07-01 14:29:59 +0300

    Switched to AUINotebook.

--- papyrus.py
+++ papyrus.py
@@ -4,15 +4,17 @@
 # This class is the main starting point for our application
 # Initializes all of our GUI code
 
-import wx, wx.richtext
-import os.path
+import wx, wx.richtext, wx.aui
+import os.path, codecs
 import scribe
+
+defaultname = '[No Name]'
 
 class MainWindow(wx.Frame):
     def __init__(self):
-        super(MainWindow, self).__init__(None, size=(800,-1))
+        super(MainWindow, self).__init__(None, size=(800, -1))
         # used as a temporary variable to store filenames, for files we are opening
-        self.filename = '[No Name]'
+        self.filename = defaultname
         self.dirname = '.'
         
         # to be appended to our file names
@@ -22,19 +24,23 @@ class MainWindow(wx.Frame):
         self.CreateInteriorWindowComponents()
         self.CreateExteriorWindowComponents()
         #self.control.SetCursor(wx.StockCursor(wx.CURSOR_POINT_LEFT))
+    
+    def NewScribe(self):
+        '''Creates a new RichTextCtrl with Scribe functionality'''
+        control = wx.richtext.RichTextCtrl(self.notebook, style=wx.TE_MULTILINE)
+        newscribe = scribe.Scribe(control)
+        #control.Bind(wx.EVT_CHAR, newscribe.OnChar)
+        return control
+
     def CreateInteriorWindowComponents(self):
         ''' Create "interior" window components. In this case it is just a
             simple multiline text control. '''
         self.panel = wx.Panel(self)
 
-        self.notebook = wx.Notebook(self.panel)
-        #self.control = wx.richtext.RichTextCtrl(self, style=wx.TE_MULTILINE)
+        self.notebook = wx.aui.AuiNotebook(self.panel)
         # create our RichTextCtrl as a child of the notebook
-        control = wx.richtext.RichTextCtrl(self.notebook, style=wx.TE_MULTILINE)
-        myscribe = scribe.Scribe(control)
-        control.Bind(wx.EVT_CHAR, myscribe.OnChar)
         # add our first page to the notebook
-        self.notebook.AddPage(control, "[No Name]")
+        self.notebook.AddPage(self.NewScribe(), defaultname)
         
         # adjust sizing parameters
         sizer = wx.BoxSizer()
@@ -56,10 +62,20 @@ class MainWindow(wx.Frame):
         toolbar.AddSimpleTool(803, wx.Bitmap('assets/img/save.png'), 'Save', 'Save the current document')
         toolbar.AddSeparator()
 
+        # add undo and redo and print
+
         toolbar.AddSimpleTool(804, wx.Bitmap('assets/img/cut.png'), 'Cut', 'Cut')
         toolbar.AddSimpleTool(805, wx.Bitmap('assets/img/page_full.png'), 'Copy', 'Copy')
-        toolbar.AddSimpleTool(806, wx.Bitmap('assets/img/package.png'), 'Paste', 'Paste')
-
+        toolbar.AddSimpleTool(806, wx.Bitmap('assets/img/glyphicons_029_paste.png'), 'Paste', 'Paste')
+        toolbar.AddSeparator()
+        
+        toolbar.AddSimpleTool(807, wx.Bitmap('assets/img/undo.png'), 'Undo', 'Undo')
+        toolbar.AddSimpleTool(808, wx.Bitmap('assets/img/redo.png'), 'Redo', 'Redo')
+        #toolbar.AddCheckTool(807, wx.Bitmap('assets/img/glyphicons_102_bold.png'))        
+        #toolbar.AddCheckTool(808, wx.Bitmap('assets/img/glyphicons_101_italic.png'))
+        #toolbar.AddCheckTool(809, wx.Bitmap('assets/img/glyphicons_103_text_underline.png'))
+        
+        # TODO: add left, center and right justified icons
         toolbar.Realize()
         
         self.Bind(wx.EVT_TOOL, self.OnNew , id=801)
@@ -68,16 +84,17 @@ class MainWindow(wx.Frame):
         self.Bind(wx.EVT_TOOL, self.OnCut,  id=804)
         self.Bind(wx.EVT_TOOL, self.OnCopy, id=805)
         self.Bind(wx.EVT_TOOL, self.OnPaste,id=806)
+        #self.Bind(wx.EVT_TOOL, self.OnBold, id=807)
 
     def CreateMenu(self):
         fileMenu = wx.Menu()
         for id, label, helpText, handler in \
-            [(wx.ID_NEW, '&New', 'New window', self.OnNew),
-             (wx.ID_NEW, '&New Tab', 'New tab', self.OnNewTab),
+            [(wx.ID_NEW, '&New Window\tCtrl+N', 'New window', self.OnNew),
+             (101, '&New Tab\tCtrl+T', 'New tab', self.OnNewTab),
              (wx.ID_ABOUT, '&About', 'Information about this program', self.OnAbout),
-             (wx.ID_OPEN, '&Open', 'Open a new file', self.OnOpen),
-             (wx.ID_SAVE, '&Save', 'Save the current file', self.OnSave),
-             (wx.ID_SAVEAS, 'Save &As', 'Save the file under a different name', self.OnSaveAs),
+             (wx.ID_OPEN, '&Open\tCtrl+O', 'Open a new file', self.OnOpen),
+             (wx.ID_SAVE, '&Save\tCtrl+S', 'Save the current file', self.OnSave),
+             (wx.ID_SAVEAS, 'Save &As\tShift+Ctrl+S', 'Save the file under a different name', self.OnSaveAs),
              (None, None, None, None),
              (wx.ID_EXIT, 'E&xit', 'Terminate the program', self.OnExit)]:
             if id == None:
@@ -88,6 +105,21 @@ class MainWindow(wx.Frame):
 
         menuBar = wx.MenuBar()
         menuBar.Append(fileMenu, '&File') # Add the fileMenu to the MenuBar
+
+        # Edit Menu
+        editMenu = wx.Menu()
+        for id, label, helpText, handler in \
+            [(wx.ID_UNDO, '&Undo\tCtrl+Z', 'Undo the previous action', self.OnUndo),
+             (wx.ID_REDO, '&Redo\tShift+Ctrl+Z', 'Redo the previous action', self.OnRedo),
+             (None, None, None, None)]:
+            if id == None:
+                editMenu.AppendSeparator()
+            else:
+                item = editMenu.Append(id, label, helpText)
+                self.Bind(wx.EVT_MENU, handler, item)
+
+        menuBar.Append(editMenu, '&Edit')
+
         self.SetMenuBar(menuBar)  # Add the menuBar to the Frame
 
     def SetTitle(self):
@@ -95,24 +127,27 @@ class MainWindow(wx.Frame):
         # call it using super:
         super(MainWindow, self).SetTitle('Papyrus')
 
-
+    
     # Helper methods:
+    
+    def GetCurrentCtrl(self):
+        '''Returns the RichTextCtrl of the currently active tab'''
+        current = self.notebook.GetSelection()
+        return self.notebook.GetPage(current)
 
     def defaultFileDialogOptions(self):
         ''' Return a dictionary with file dialog options that can be
             used in both the save file dialog as well as in the open
             file dialog. '''
         return dict(message='Choose a file', defaultDir=self.dirname,
-                    wildcard='*.txt')
+                    wildcard='*'+self.extension)
 
     def askUserForFilename(self, **dialogOptions):
         dialog = wx.FileDialog(self, **dialogOptions)
         if dialog.ShowModal() == wx.ID_OK:
             userProvidedFilename = True
 
-            #current = self.notebook.GetSelection()
-            #self.notebook.SetPageText(current, dialog.GetFilename())
-            self.filename = dialog.GetFilename()+self.extension
+            self.filename = dialog.GetFilename()
             self.dirname = dialog.GetDirectory()
 
         else:
@@ -124,15 +159,11 @@ class MainWindow(wx.Frame):
     def OnNew(self, event):
         # TODO: add support for tabbed browsing
         frame = MainWindow()
-        frame.Centre()
         frame.Show()
     
     def OnNewTab(self, event):
-        control = wx.richtext.RichTextCtrl(self.notebook, style=wx.TE_MULTILINE)
-        myscribe = scribe.Scribe(control)
-        control.Bind(wx.EVT_CHAR, myscribe.OnChar)
         # add our new page to the notebook
-        self.notebook.AddPage(control, "[No Name]")
+        self.notebook.AddPage(self.NewScribe(), defaultname)
 
     def OnAbout(self, event):
         dialog = wx.MessageDialog(self, 'A text editor inspired by Google Scribe', 'About Papyrus Editor', wx.OK)
@@ -143,37 +174,35 @@ class MainWindow(wx.Frame):
         self.Close()  # Close the main window.
 
     def OnSave(self, event):
-        control = self.notebook.GetCurrentPage()
         current = self.notebook.GetSelection()
         filename = self.notebook.GetPageText(current)
-        if filename == "[No Name]":
+        control = self.notebook.GetPage(current)
+        if filename == defaultname:
             if self.askUserForFilename(defaultFile=filename, style=wx.SAVE, **self.defaultFileDialogOptions()):    
                 # get the updated filename
-                textfile = open(os.path.join(self.dirname, self.filename), 'w')
+                textfile = codecs.open(os.path.join(self.dirname, self.filename+self.extension), 'w', 'utf-8', 'strict')
                 textfile.write(control.GetValue())
                 textfile.close()
-
-                self.notebook.SetPageText(current, self.filename)
+                #control.SaveFile(os.path.join(self.dirname, self.filename+self.extension))
+                self.notebook.SetPageText(current, self.filename+self.extension)
         else:
-            textfile = open(os.path.join(self.dirname, filename), 'w')
+            textfile = codecs.open(os.path.join(self.dirname, filename), 'w','utf-8', 'strict')
             textfile.write(control.GetValue())
             textfile.close()
+            #control.SaveFile(os.path.join(self.dirname, filename))
+
 
     def OnOpen(self, event):
         if self.askUserForFilename(style=wx.OPEN,
                                    **self.defaultFileDialogOptions()):
 
-            control = wx.richtext.RichTextCtrl(self.notebook, style=wx.TE_MULTILINE)
-            myscribe = scribe.Scribe(control)
-            control.Bind(wx.EVT_CHAR, myscribe.OnChar)
             # add our first page to the notebook
-            self.notebook.AddPage(control, self.filename)
+            self.notebook.AddPage(self.NewScribe(), self.filename)
 
-            #filename = self.notebook.GetPageText(current)
-                       
             textfile = open(os.path.join(self.dirname, self.filename), 'r')
             control.SetValue(textfile.read())
             textfile.close()
+            #control.LoadFile(os.path.join(self.dirname,self.filename))
 
     def OnSaveAs(self, event):
         current = self.notebook.GetSelection()
@@ -184,30 +213,28 @@ class MainWindow(wx.Frame):
             self.OnSave(event)
 
     def OnCut(self, event):
-        self.control.Cut()
+        control = self.GetCurrentCtrl()
+        control.Cut()
 
     def OnCopy(self, event):
-        self.control.Copy()
+        control = self.GetCurrentCtrl()
+        control.Copy()
 
     def OnPaste(self, event):
-        self.control.Paste()        
+        control = self.GetCurrentCtrl()
+        control.Paste()        
 
+    def OnBold(self, event):
+        control = self.GetCurrentCtrl()
+        control.BeginBold()
+
+    def OnUndo(self, event):
+        pass
+    def OnRedo(self, event):
+        pass
 app = wx.App(redirect=False)
 frame = MainWindow()
 app.SetTopWindow(frame)
 frame.Centre()
-frame.Show(True)
+frame.Show()
 app.MainLoop()
-
-# class MyApp(wx.App):
-#     def OnInit(self):
-#         frame = MainWindow()
-#         self.SetTopWindow(frame)
-# 
-#         print "Print statements go to this stdout window by default."
-# 
-#         frame.Show(True)
-#         return True
-#         
-# app = MyApp(redirect=True)
-# app.MainLoop()
